0409338d32873bfe8da58ad88ff97a6b69ad8f43
Rich Johnson <rich.johnson@monsanto.com>
2016-09-04 16:56:50 -0500

    update todos

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -7,46 +7,25 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/to
 var client = new pg.Client(connectionString);
 client.connect();
 
-todos.get('/',function(req, res) {
-	  
+todos.get('/',function(req, res, next) {	  
 	console.log(req.user.email)
 	console.log("GETTING TODOS");
-    var results = [];
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
-		
         // Handle connection errors
         if(err) {
           done();
           console.log(err);
           return res.status(500).json({ success: false, data: err});
         }
-
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items WHERE email=$1 ORDER BY id ASC;", [req.user.email]);
-
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
-
+		return getUsersTodos(req, res, done);
     });
-
 });
 
 todos.post('/', function(req, res, next) {
 	console.log(req.user.email)
-	console.log(req.user.id)
 	console.log("POSTING TODOS");
-    var results = [];
-
     // Grab data from http request
     console.log(req.body);
     var data = {text: req.body.text, complete: false};
@@ -63,27 +42,12 @@ todos.post('/', function(req, res, next) {
         // SQL Query > Insert Data
         client.query("INSERT INTO items(text, complete, email) values($1, $2, $3)", [data.text, data.complete, req.user.email]);
 
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items WHERE email=$1 ORDER BY id ASC;", [req.user.email]);
-
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
-
-
+		return getUsersTodos(req, res, done);
     });
 });
 
 todos.put('/:todo_id', function(req, res) {
 	console.log("PUTTING TODOS");
-    var results = [];
 
     // Grab data from the URL parameters
     var id = req.params.todo_id;
@@ -99,34 +63,18 @@ todos.put('/:todo_id', function(req, res) {
           console.log(err);
           return res.status(500).send(json({ success: false, data: err}));
         }
-
         // SQL Query > Update Data
         client.query("UPDATE items SET text=($1), complete=($2) WHERE id=($3)", [data.text, data.complete, id]);
 
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items WHERE email=$1 ORDER BY id ASC;", [req.user.email]);
-
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
+        return getUsersTodos(req, res, done);
     });
-
 });
 
 todos.delete('/:todo_id', function(req, res) {
 	console.log("DELETING TODOS");
-    var results = [];
 
     // Grab data from the URL parameters
     var id = req.params.todo_id;
-
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -140,21 +88,26 @@ todos.delete('/:todo_id', function(req, res) {
         // SQL Query > Delete Data
         client.query("DELETE FROM items WHERE id=($1)", [id]);
 
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items WHERE email=$1 ORDER BY id ASC;", [req.user.email]);
+        return getUsersTodos(req, res, done);
+    });
+});
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
+function getUsersTodos(req, res, done) {
+	var results = [];
+	
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items WHERE email=$1 ORDER BY id ASC;", [req.user.email]);
 
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+        results.push(row);
     });
 
-});
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+        done();
+        return res.json(results);
+    });
+}
 
 module.exports = todos;
