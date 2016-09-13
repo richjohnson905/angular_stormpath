87290f001e63232528cc91faa758b69871df5e8c
Rich Johnson <rich.johnson@monsanto.com>
2016-09-13 08:47:54 -0500

    db working but no user

--- server/api/todo/todo.controller.js
+++ server/api/todo/todo.controller.js
@@ -12,5 +12,38 @@ var _ = require('lodash');
 
 // Get list of todos
 exports.index = function(req, res) {
-  res.json([{name: "one"},{name: "two"},{name: "three"},{name: "four"}]);
+  // res.json([{name: "one"},{name: "two"},{name: "three"},{name: "four"}]);
+  getStormId(req, res, function(storm_id) {
+  		return getUsersTodos(req, res, storm_id);
+  	});
 };
+
+function getUsersTodos(req, res, storm_id) {
+	var results = [];
+    // SQL Query > Select Data
+    var query = req.database.query("SELECT * FROM items WHERE stormpath_id=$1 ORDER BY id ASC;", [storm_id]);
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+        results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+        return res.json(results);
+    });
+}
+
+function getStormId(req, res, callback) {
+	var storm_id = 0;
+	var query = req.database.query("SELECT * FROM stormpath WHERE email=$1;", ["eve@msn.com"]); //[req.user.email]);
+	
+    query.on('row', function(row) {
+        storm_id = row.id;
+    });
+	
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+        callback(storm_id);
+    });
+}
--- server/app.js
+++ server/app.js
@@ -11,21 +11,66 @@ var express = require('express');
 var ExpressStormpath = require('express-stormpath');
 var path = require('path');
 var config = require('./config/environment');
+var pg = require('pg');
+
+var db;
+
+//var connectionString = 'postgres://ec2-54-243-47-213.compute-1.amazonaws.com:5432/dbboidjdr156ff';
+if (true) {
+	console.log("setting up local db");
+	var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+	var db = new pg.Client(connectionString);
+	db.connect();
+}
+else {
+	console.log("setting up ssl aws db");
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, theClient) {
+	  if (err) throw err;
+	  console.log('Connected to postgres! Getting schemas...');
+	  db = theClient;
+	});
+}
+
+
 // Setup server
 var app = express();
 
+
+
+app.all('*', function(request, response, next) {
+    request.database = db;
+    next();
+});
+
 app.use(ExpressStormpath.init(app,{
-  web: {
-    spa: {
-      enabled: true,
-      view: path.join(__dirname, '..','client','index.html')
-    }
-  }
+	postRegistrationHandler: function (account, req, res, next) {
+		    console.log('User:', account.email, 'just registered!');
+			db.query("INSERT INTO stormpath(email) values($1)", [account.email]);
+		    next();
+		},
+	web: {
+		spa: {
+		  enabled: true,
+		  view: path.join(__dirname, '..','client','index.html')
+		},
+		me: {
+			expand: {
+				customData: true,
+				groups: true
+				}
+			},
+		debug: 'info'
+	}
 }));
 
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
+
+app.on('stormpath.ready',function () {
+  console.log('Stormpath Ready');
+});
 
 // Start server
 server.listen(config.port, config.ip, function () {
