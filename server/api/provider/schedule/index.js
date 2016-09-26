58344b047d490a7216838bfcec946be4b10e9627
Rich Johnson <rich.johnson@monsanto.com>
2016-09-26 13:05:06 -0500

    repeat true

--- server/api/provider/schedule/index.js
+++ server/api/provider/schedule/index.js
@@ -5,9 +5,6 @@ var controller = require('./schedule.controller');
 
 var schedules = express.Router({mergeParams: true});
 
-// var providerRouter = require('../index');
-// providerRouter.use('/:pid/schedules', schedules);
-
 schedules.get('/', controller.index);
 schedules.get('/:sid', controller.show);
 schedules.post('/', controller.create);
