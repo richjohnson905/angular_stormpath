a1c878e9578a9a9e54d2cfbe916e56dd6056e1a5
Rich Johnson <rich.johnson@monsanto.com>
2016-09-30 14:47:29 -0500

    wip

--- server/models/todo.js
+++ server/models/todo.js
@@ -4,8 +4,6 @@ module.exports = function(sequelize, DataTypes) {
     var Todo = sequelize.define('Todo', {
         name: DataTypes.STRING,
         complete: DataTypes.BOOLEAN,
-        foo: DataTypes.STRING,
-        bar: Datatypes.STRING,
         stormId: DataTypes.STRING
     });
     return Todo;
